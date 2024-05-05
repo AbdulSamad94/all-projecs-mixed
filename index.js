f7019a86bf3cbb2a0d50bc06a281e2029a04234f
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-05-05 16:44:36 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ while (mainCondition) {
             name: "select",
             type: "list",
             message: message1,
-            choices: ["1)   CLI-Calculator", "2)   Number Guessing Game", "3)   ATM-Machine", "4)   Todo-list", "5)   Currency-Converter", "6)   Word-Counter", "7)   Rock-Paper-Scissor", "8)   Quiz"]
+            choices: ["1)   CLI-Calculator", "2)   Number Guessing Game", "3)   ATM-Machine", "4)   Todo-list", "5)   Currency-Converter", "6)   Word-Counter", "7)   Rock-Paper-Scissor", "8)   Quiz", "9)   Student-Management-System"]
         }]);
     let select = selectOption.select;
     if (select == "1)   CLI-Calculator") {
@@ -603,6 +603,84 @@ while (mainCondition) {
         }
         else {
             console.log(chalk.red.bold("Not a problem!!"));
+        }
+    }
+    else if ("9)   Student-Management-System") {
+        mainCondition = false;
+        console.log(chalk.yellowBright("\n--------------------------Welcome to the Student-Management project by Abdul Samad------------------------\n"));
+        let randomId = Math.floor(Math.random() * 9999 + 1000);
+        let nameasking = await inquirer.prompt([{
+                name: "name",
+                type: "input",
+                message: chalk.blueBright.bold.underline("Enter Your Name:")
+            }]);
+        let selectCourse = await inquirer.prompt([{
+                name: "course",
+                type: "list",
+                message: chalk.blueBright.bold.underline("Select Any One Of The Course : "),
+                choices: ["TypeScript", "JavaScript", "Python", "Java", "PHP"]
+            }]);
+        let coursefees = {
+            "TypeScript": 4000,
+            "JavaScript": 4200,
+            "Python": 3500,
+            "Java": 3000,
+            "PHP": 3800,
+        };
+        let pinCodes = {
+            EasyPaisa: 123,
+            JazzCash: 1234,
+            PayPal: 12345
+        };
+        selectCourse = selectCourse.course;
+        console.log(chalk.redBright.bold(`\nYou Have Selected ${chalk.white.underline(selectCourse)}, And It's Fees is ${chalk.white.bold.underline(coursefees[selectCourse])}\n`));
+        let selectBank = await inquirer.prompt([{
+                name: "bank",
+                type: "list",
+                message: chalk.yellowBright.bold("Select Any One Of The Bank To Proceed..."),
+                choices: ["EasyPaisa", "JazzCash", "PayPal"]
+            }]);
+        selectBank = selectBank.bank;
+        let pinCheck = await inquirer.prompt([{
+                name: "pin",
+                type: "input",
+                message: chalk.redBright.bold.underline(`Enter The pinCode Of ${selectBank}`)
+            }]);
+        pinCheck = pinCheck.pin;
+        let balace = 10000;
+        if (pinCheck == pinCodes[selectBank]) {
+            console.log(chalk.greenBright.bold(`\nYour Balance in ${selectBank} is ${balace}..\n`));
+            let enterAmount = await inquirer.prompt([{
+                    name: "amount",
+                    type: "number",
+                    message: chalk.yellow.bold(`Enter The Amount Of the ${chalk.white.underline(selectCourse)} : `)
+                }]);
+            if (enterAmount.amount == coursefees[selectCourse]) {
+                console.log(chalk.green.bold(`\nCongratulations! You Have brought ${chalk.blue.underline(selectCourse)} Course..\n`));
+                let exitMode = await inquirer.prompt([{
+                        name: "exit",
+                        type: "list",
+                        message: chalk.yellow.bold("Select Any One Of The Exit Mode :"),
+                        choices: ["View Final Status", "Exit"]
+                    }]);
+                if (exitMode.exit == "View Final Status") {
+                    console.log(chalk.white.bold(`\n ---------------AdmitCard---------------\n`));
+                    console.log(chalk.green.bold(`Student Name : ${nameasking.name}`));
+                    console.log(chalk.green.bold(`Student's Id : ${randomId}`));
+                    console.log(chalk.green.bold(`Selected Course : ${selectCourse}`));
+                    console.log(chalk.green.bold(`Course Fees : ${coursefees[selectCourse]}`));
+                    console.log(chalk.green.bold(`Course Fees Paid? : Paid `));
+                }
+                else {
+                    console.log(chalk.blue(`\n--------------------Exiting------------------\n`));
+                }
+            }
+            else {
+                console.log(chalk.red.bold.underline("\nPlease Enter The Valid Amount\n"));
+            }
+        }
+        else {
+            console.log(chalk.redBright.bold("\n Wrong PinCode!!\n"));
         }
     }
 }
